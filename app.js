a9d0f4332948a19b2f0438c9cf09144bf5bf9bf6
Nick Wong <hrpearl01@gmail.com>
2014-04-22 20:08:26 +0100

    added email signup

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var mongo = require('mongodb');
 var monk = require('monk');
 var path = require('path');
 var routes = require('./routes');
-var user = require('./routes/user');
 var app = express();
 var auth = require('./scripts/auth');
 var db = require('./scripts/data');
@@ -62,11 +61,33 @@ app.post('/kimono_spitzer', function(req, res) {
 });
 
 app.post('/signup', function(req, res) {
-	var SignUp = new auth.signup(req.body.username, req.body.password, req.body.email, res);
+	// var SignUp = new auth.signup(req.body.username, req.body.password, req.body.email, res);
+	var Parse = require('parse').Parse;
+	Parse.initialize(process.env.parseID, process.env.parseJavascriptKey, process.env.parseMasterKey);
+
+	var user = new Parse.User();
+
+	user.set("username", req.body.username);
+	user.set("password", req.body.password);
+	user.set("email", req.body.email);
+
+	user.signUp(null, {
+		success: function(user) {
+			// Redirect to email confirmation page
+			res.render('email_confirmation', { email: req.body.email});
+		},
+		error: function(user, error) {
+			// Show the error message somewhere and let the user try again.
+			console.log("Error: " + error.code + " " + error.message);
+			if(error == 202) {
+				window.document.getElementById('emsg').innerHTML=error.message;
+			}
+		}
+	});
 });
 
 app.get('/verify_email', function(req, res) {
-	res.render('user', { user_email: req.params.email });
+	res.render('user', { email: req.params.email });
 });
 
 app.post('/upload', function(req, res) {
